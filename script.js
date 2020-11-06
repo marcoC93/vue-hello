da54272e46def29e1a9150ced569aa7958fd91ce
Marco <marco.chianese93@gmail.com>
2020-11-06 16:10:42 +0100

    Stampa scritta su schermo

---
+++ script.js
@@ -0,0 +1,15 @@
+// stampare a schermo un messaggio all'interno di un h1 utilizzando data
+
+//Bonus: Aggiungere un'immagine presa anch'essa da una data.
+
+
+
+var app = new Vue({
+  el: '#root',
+
+
+  data: {
+    message: 'Hello Vue!',
+    imgLink : 'https://pixabay.com/get/57e7d245435ba914f1dc8460da29317e1139d9e0515075_640.png'
+  }
+})
